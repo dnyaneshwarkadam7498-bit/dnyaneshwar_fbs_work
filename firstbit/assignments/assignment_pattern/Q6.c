d821cc05fa7bdaf574a9ab85f14b43546efcf7e9
dnyaneshwarkadam7498-bit <dnyaneshwarkadam7498@gmail.com>
2026-08-07 22:19:17 +0530

    Add files via upload

---
+++ firstbit/assignments/assignment_pattern/Q6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        
+        for(j=1;j<=5;j++){
+            if(i==j || i==1 || j==5 || i==5 || j==1 || i+j==6)
+         printf("* ");
+         else
+         printf("  ");
+    }
+        printf("\n");
+    }
+}
